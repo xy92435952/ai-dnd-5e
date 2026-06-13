9e662b18e7635b290aa0670d3c22415341884779
yao xiao <xy92435952@gmail.com>
2026-06-14 06:56:28 +0800

    Add higher-level spell learning regressions

--- backend/tests/unit/test_character_leveling_service.py
+++ backend/tests/unit/test_character_leveling_service.py
@@ -1,7 +1,8 @@
 import pytest
 
-from services.dnd_rules import calc_derived
+from services.dnd_rules import calc_derived, get_cantrips_count
 from services import character_leveling_service
+from services.spell_service import spell_service
 
 
 BASE_SCORES = {"str": 16, "dex": 14, "con": 14, "int": 12, "wis": 12, "cha": 14}
@@ -59,6 +60,60 @@ def _advance_character_levels(
         class_resources = update["class_resources"]
 
     return update
+
+
+def _progression_count(table, level):
+    count = 0
+    for threshold, value in sorted(table.items()):
+        if level >= threshold:
+            count = value
+    return count
+
+
+def _max_spell_rank(spell_slots_max):
+    slot_levels = {
+        "1st": 1,
+        "2nd": 2,
+        "3rd": 3,
+        "4th": 4,
+        "5th": 5,
+        "6th": 6,
+        "7th": 7,
+        "8th": 8,
+        "9th": 9,
+    }
+    return max(
+        [
+            slot_levels.get(slot_key, 0)
+            for slot_key, count in (spell_slots_max or {}).items()
+            if int(count or 0) > 0
+        ]
+        or [0]
+    )
+
+
+def _class_spell_details(char_class):
+    return [
+        {"name": spell["name"], "level": spell.get("level", 0)}
+        for spell in spell_service.get_for_class(char_class)
+        if spell.get("level", 0) > 0
+    ]
+
+
+def _legal_spell_names(char_class, max_level):
+    return [
+        spell["name"]
+        for spell in _class_spell_details(char_class)
+        if spell.get("level", 0) <= max_level
+    ]
+
+
+def _class_cantrip_names(char_class):
+    return [
+        spell["name"]
+        for spell in spell_service.get_for_class(char_class)
+        if spell.get("level", 0) == 0
+    ]
 
 
 def test_build_level_up_update_adds_new_spell_slots_without_refilling_spent_slots():
@@ -596,6 +651,113 @@ def test_build_level_up_update_uses_known_caster_progression_capacity():
 
     assert update["new_level"] == 11
     assert update["known_spells"] == ["Hellish Rebuke", "Hex"]
+
+
+@pytest.mark.parametrize(
+    "char_class,old_level",
+    [
+        ("Bard", 9),
+        ("Ranger", 8),
+        ("Sorcerer", 9),
+        ("Warlock", 12),
+    ],
+)
+def test_known_casters_follow_higher_level_spell_learning_progression_with_real_spell_lists(
+    char_class,
+    old_level,
+):
+    ability_scores = {"str": 10, "dex": 14, "con": 14, "int": 10, "wis": 16, "cha": 16}
+    old_derived = calc_derived(char_class, old_level, ability_scores, None, race="Human")
+    new_derived = calc_derived(char_class, old_level + 1, ability_scores, None, race="Human")
+    old_known_count = _progression_count(
+        character_leveling_service.SPELLS_KNOWN[char_class],
+        old_level,
+    )
+    new_known_count = _progression_count(
+        character_leveling_service.SPELLS_KNOWN[char_class],
+        old_level + 1,
+    )
+    learn_count = new_known_count - old_known_count
+    old_cantrip_count = get_cantrips_count(char_class, old_level)
+    new_cantrip_count = get_cantrips_count(char_class, old_level + 1)
+    cantrip_gain = new_cantrip_count - old_cantrip_count
+    legal_spells = _legal_spell_names(
+        char_class,
+        _max_spell_rank(new_derived.get("spell_slots_max", {})),
+    )
+    cantrip_names = _class_cantrip_names(char_class)
+    known_spells = legal_spells[:old_known_count]
+    learned_spells = legal_spells[old_known_count:old_known_count + learn_count]
+    cantrips = cantrip_names[:old_cantrip_count]
+    learned_cantrips = cantrip_names[old_cantrip_count:old_cantrip_count + cantrip_gain]
+
+    update = character_leveling_service.build_level_up_update(
+        char_class=char_class,
+        level=old_level,
+        ability_scores=ability_scores,
+        derived=old_derived,
+        hp_current=old_derived["hp_max"],
+        spell_slots=dict(old_derived.get("spell_slots_max", {})),
+        use_average_hp=True,
+        known_spells=known_spells,
+        cantrips=cantrips,
+        learned_spells=learned_spells,
+        learned_cantrips=learned_cantrips,
+        available_class_spells=_class_spell_details(char_class),
+        available_class_cantrips=cantrip_names,
+        race="Human",
+    )
+
+    assert learn_count > 0
+    assert update["new_level"] == old_level + 1
+    assert update["known_spells"] == [*known_spells, *learned_spells]
+    assert update["learned_spells"] == learned_spells
+    assert len(update["known_spells"]) == new_known_count
+    if cantrip_gain:
+        assert update["cantrips"] == [*cantrips, *learned_cantrips]
+        assert update["learned_cantrips"] == learned_cantrips
+
+
+def test_wizard_higher_level_spellbook_and_cantrip_progression_uses_real_options():
+    ability_scores = {"str": 8, "dex": 14, "con": 14, "int": 18, "wis": 12, "cha": 10}
+    old_level = 9
+    old_derived = calc_derived("Wizard", old_level, ability_scores, None, race="Human")
+    new_derived = calc_derived("Wizard", old_level + 1, ability_scores, None, race="Human")
+    legal_spells = _legal_spell_names(
+        "Wizard",
+        _max_spell_rank(new_derived.get("spell_slots_max", {})),
+    )
+    cantrip_names = _class_cantrip_names("Wizard")
+    old_cantrip_count = get_cantrips_count("Wizard", old_level)
+    new_cantrip_count = get_cantrips_count("Wizard", old_level + 1)
+    known_spells = legal_spells[:8]
+    learned_spells = legal_spells[8:10]
+    cantrips = cantrip_names[:old_cantrip_count]
+    learned_cantrips = cantrip_names[old_cantrip_count:new_cantrip_count]
+
+    update = character_leveling_service.build_level_up_update(
+        char_class="Wizard",
+        level=old_level,
+        ability_scores=ability_scores,
+        derived=old_derived,
+        hp_current=old_derived["hp_max"],
+        spell_slots=dict(old_derived.get("spell_slots_max", {})),
+        use_average_hp=True,
+        known_spells=known_spells,
+        cantrips=cantrips,
+        learned_spells=learned_spells,
+        learned_cantrips=learned_cantrips,
+        available_class_spells=_class_spell_details("Wizard"),
+        available_class_cantrips=cantrip_names,
+        race="Human",
+    )
+
+    assert update["new_level"] == 10
+    assert update["known_spells"] == [*known_spells, *learned_spells]
+    assert update["learned_spells"] == learned_spells
+    assert update["cantrips"] == [*cantrips, *learned_cantrips]
+    assert update["learned_cantrips"] == learned_cantrips
+    assert update["preparation_type"] == "spellbook"
 
 
 def test_build_level_up_update_replaces_known_caster_spell():
